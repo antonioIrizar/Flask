e4a5908547c41493be58ded3574fd02b029fd6df
antonioIrizar <antonioirizar@gmail.com>
2014-07-22 10:50:59 +0200

    update requerimets for flask-testing, start with flask-testing, it don't work

--- videoclub/videoclub_test.py
+++ videoclub/videoclub_test.py
@@ -1,39 +1,66 @@
 import os
 import videoclub
-from videoclub import db,Flask,User
+from videoclub import db,Flask,User,app
 import unittest
 import tempfile
 
-class FlaskrTestCase(unittest.TestCase):
+class VideoclubTestCase(unittest.TestCase):
+    """
+    def setUp(self):
+        #self.app=Flask(__name__)
+        #db.init_app()
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+            test_user = User(name="test", password="test")
+            db.session.add(test_user)
+            db.session.commit()
 
-	def setUp(self):
-		self.app=Flask(__name__)
-		db.init_app(self.app)
-		with self.app.app_context():
-			db.drop_all()
-			db.create_all()
-			test_user = User(name="test", password="test")
-			db.session.add(test_user)
-			db.session.commit()
+    def tearDown(self):
+        #self.app = Flask(__name__)
+        #db.init_app(self.app)
+        with app.app_context():
+            db.drop_all()
+    """
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['CSRF_ENABLED'] = False
+        app.config['DATABASE_FILE'] = 'test.sqlite'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
+        self.app = app.test_client()
+        db.create_all()
+        test_user = User(name="test", password="test")
+        db.session.add(test_user)
+        db.session.commit()
 
-	def tearDown(self):
-		self.app = Flask(__name__)
-		db.init_app(self.app)
-		with self.app.app_context():
-			db.drop_all()
 
-	def login(self,username,password):
-		return self.app.post('/login/', data=dict(username=username,password=password), follow_redirects=True)
+    def tearDown(self):
+        db.drop_all()
 
-	def logout(self):
-		return self.app.get('/admin/logout/', follow_redirects=True)
+    """
+    def setUp(self):
+        self.db_fd, videoclub.app.config['DATABASE'] = tempfile.mkstemp()
+        videoclub.app.config['TESTING'] = True
+        self.app = videoclub.app.test_client()
+        videoclub.db.create_all
 
-	def test_login_logout(self):
-		rv = self.login(videoclub.app.config['username'],
-                        videoclub.app.config['password'])
-		assert 'You were logged in' in rv.data
-		rv = self.logout()
-		assert 'You were logged out' in rv.data
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(videoclub.app.config['DATABASE'])
+
+    """
+    def login(self,username,password):
+        return app.post('/admin/login/', data=dict(username=username,password=password), follow_redirects=True)
+
+    def logout(self):
+        return app.get('/admin/logout/', follow_redirects=True)
+
+    def test_login_logout(self):
+        rv = self.login('test',
+                        'test')
+        assert 'You were logged in' in rv.data
+        rv = self.logout()
+        assert 'You were logged out' in rv.data
 
 if __name__ == '__main__':
-		unittest.main()
+        unittest.main()
